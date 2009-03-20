2db7e8b8f2cab85735cab30d687cf1777bad7380
Leif Walsh <leif.walsh@gmail.com>
2009-03-20 19:46:44 -0400

    Reverted to old version of container_of

--- util.h
+++ util.h
@@ -1,3 +1,6 @@
+#ifndef _UTIL_H_
+#define _UTIL_H_
+
 #undef offsetof
 #ifdef __compiler_offsetof
 #define offsetof(type, member) __compiler_offsetof(type, member)
@@ -5,19 +8,8 @@
 #define offsetof(type, member) ((size_t) &((type *)0)->member)
 #endif
 
-/**
- * container_of - cast a member of a structure out to the containing structure
- * @ptr:    the pointer to the member
- * @type:   the type of the container struct this is embedded in
- * @member: the name of the member within the struct
- *
- * (stolen from linux kernel)
- */
-#if 0
 #define container_of(ptr, type, member) ({ \
 	    const typeof( ((type *)0)->member ) *__mptr = (ptr); \
 	    (type *)( (char *)__mptr - offsetof(type, member) );})
-#else
-#define container_of(ptr, type, member) \
-	((type *)( (char *) ptr - offsetof(type, member) ))
-#endif
+
+#endif  /* _UTIL_H_ */
