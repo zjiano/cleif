1d8b64ce0fb1ea7270c95157e8733dbafa6328d1
Leif Walsh <leif.walsh@gmail.com>
2009-09-18 01:38:53 -0400

    wrote better withstmt test

--- withstmt.h
+++ withstmt.h
@@ -3,9 +3,9 @@
 
 #define with(lock, lock_fn, unlock_fn, block)   \
     do {                                        \
-    lock_fn(&lock);                             \
-    (block);                                    \
-    unlock_fn(&lock);                           \
+        lock_fn(&lock);                         \
+        (block);                                \
+        unlock_fn(&lock);                       \
     } while(0)
 
 #endif  /* __WITHSTMT_H__ */
--- withstmt_test.c
+++ withstmt_test.c
@@ -1,67 +1,89 @@
-#include <stdio.h>
+#include <assert.h>
 
-#include <error.h>
-#include <test.h>
+#include <unittest.h>
 #include <withstmt.h>
 
 struct null_lock {
     int id, num_locks, num_unlocks;
 };
 
-void nl_lock(struct null_lock *lock) {
-    lock->num_locks++;
-    printf("Locking lock %d (now %d locks, %d unlocks).\n",
-           lock->id, lock->num_locks, lock->num_unlocks);
+static inline void nl_init(struct null_lock *lock, int id) {
+    lock->num_locks = lock->num_unlocks = 0;
+    lock->id = id;
 }
 
-void nl_unlock(struct null_lock *lock) {
+static void nl_lock(struct null_lock *lock) {
+    lock->num_locks++;
+}
+
+static void nl_unlock(struct null_lock *lock) {
     lock->num_unlocks++;
-    printf("Unlocking lock %d (now %d locks, %d unlocks).\n",
-           lock->id, lock->num_locks, lock->num_unlocks);
+}
+
+static void test_single_lock(void) {
+    struct null_lock lock;
+
+    nl_init(&lock, 1);
+
+    expect(lock.num_locks == 0);
+    expect(lock.num_unlocks == 0);
+
+    with(lock, nl_lock, nl_unlock, {
+            assert(lock.num_locks == 1);
+            assert(lock.num_unlocks == 0);
+        });
+
+    assert(lock.num_locks == 1);
+    assert(lock.num_unlocks == 1);
+}
+
+static void test_double_lock(void) {
+    struct null_lock lock1, lock2;
+
+    nl_init(&lock1, 1);
+    nl_init(&lock2, 2);
+
+    expect(lock1.num_locks == 0);
+    expect(lock1.num_unlocks == 0);
+    expect(lock2.num_locks == 0);
+    expect(lock2.num_unlocks == 0);
+
+    with(lock1, nl_lock, nl_unlock, {
+            assert(lock1.num_locks == 1);
+            assert(lock1.num_unlocks == 0);
+        });
+
+    assert(lock1.num_locks == 1);
+    assert(lock1.num_unlocks == 1);
+
+    with(lock2, nl_lock, nl_unlock, {
+            assert(lock2.num_locks == 1);
+            assert(lock2.num_unlocks == 0);
+            expect(lock1.num_locks == 1);
+            expect(lock1.num_unlocks == 1);
+
+            with(lock1, nl_lock, nl_unlock, {
+                    expect(lock2.num_locks == 1);
+                    expect(lock2.num_unlocks == 0);
+                    assert(lock1.num_locks == 2);
+                    assert(lock1.num_unlocks == 1);
+                });
+
+            expect(lock2.num_locks == 1);
+            expect(lock2.num_unlocks == 0);
+            assert(lock1.num_locks == 2);
+            assert(lock1.num_unlocks == 2);
+        });
+
+    assert(lock2.num_locks == 1);
+    assert(lock2.num_unlocks == 1);
+    assert(lock1.num_locks == 2);
+    assert(lock1.num_unlocks == 2);
 }
 
 int main(void) {
-    struct null_lock lock1, lock2;
-
-    lock1.num_locks = lock1.num_unlocks =
-        lock2.num_locks = lock2.num_unlocks = 0;
-    lock1.id = 1;
-    lock2.id = 2;
-
-    printf("Start!\n");
-
-    with(lock1, nl_lock, nl_unlock, {
-            _assert(lock1.num_locks == 1);
-            _assert(lock1.num_unlocks == 0);
-            printf("Look, ma!  Lock 1 is locked!\n");
-        });
-
-    _assert(lock1.num_locks == 1);
-    _assert(lock1.num_unlocks == 1);
-
-    printf("Unlocked!\n");
-
-    with(lock2, nl_lock, nl_unlock, {
-            _assert(lock2.num_locks == 1);
-            _assert(lock2.num_unlocks == 0);
-            printf("Lock 2 locked.\n");
-
-            with(lock1, nl_lock, nl_unlock, {
-                    _assert(lock1.num_locks == 2);
-                    _assert(lock1.num_unlocks == 1);
-                    printf("Lock 1 also locked.\n");
-                });
-            _assert(lock2.num_locks == 1);
-            _assert(lock2.num_unlocks == 0);
-            printf("Lock 1 unlocked.\n");
-        });
-    _assert(lock2.num_locks == 1);
-    _assert(lock2.num_unlocks == 1);
-
-    _assert(lock1.num_locks == 2);
-    _assert(lock1.num_unlocks == 2);
-
-    printf("Unlocked again!\n");
+    TEST(single_lock);
+    TEST(double_lock);
 
     return 0;
 }
